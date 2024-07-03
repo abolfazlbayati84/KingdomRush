fc37563f261ddf3ad74c1a46a3457e6fd92345a3
Abolfazl Bayati <abolfazlbayati60@gmail.com>
2024-07-03 23:52:27 +0330

    login and signup are completed

---
+++ KingdomRush/src/main/java/org/example/kingdomrush/HomeController.java
@@ -0,0 +1,80 @@
+package org.example.kingdomrush;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+
+public class HomeController {
+
+    @FXML
+    private ImageView diamondImage;
+
+    @FXML
+    private Label diamondLabel;
+
+    @FXML
+    private Circle firstCircle;
+
+    @FXML
+    private Label firstLabel;
+
+    @FXML
+    private Circle fourthCircle;
+
+    @FXML
+    private Label fourthLabel;
+
+    @FXML
+    private AnchorPane initialAnchor;
+
+    @FXML
+    private Circle secondCircle;
+
+    @FXML
+    private Label secondLabel;
+
+    @FXML
+    private ImageView settingsImage;
+
+    @FXML
+    private ImageView shopImage;
+
+    @FXML
+    private Circle thirdCircle;
+
+    @FXML
+    private Label thirdLabel;
+
+    @FXML
+    void firstLevelAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void fourthLevelAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void secondLevelAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void thirdLevelAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void settingsAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void shopAction(MouseEvent event) {
+
+    }
+}
---
+++ KingdomRush/src/main/java/org/example/kingdomrush/ShopPageController.java
@@ -0,0 +1,4 @@
+package org.example.kingdomrush;
+
+public class ShopPageController {
+}
